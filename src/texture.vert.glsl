e79b9a26941298e614120cbaa7a605854aa95eab
prodbysky <kyrbynyt@gmail.com>
2024-03-02 10:10:16 +0200

    MVP model in texture shader

--- src/texture.vert.glsl
+++ src/texture.vert.glsl
@@ -3,9 +3,13 @@
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec2 texCoord;
 
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+
 out vec2 TexCoord;
 
 void main() {
-    gl_Position = vec4(pos, 1.0f);
+    gl_Position = projection * view * model * vec4(pos, 1.0);
     TexCoord = texCoord;
 }
