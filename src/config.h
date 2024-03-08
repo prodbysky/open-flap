2084bd71f8a62336e6a0fea5ac09db3df6977cc4
prodbysky <kyrbynyt@gmail.com>
2024-03-09 01:13:31 +0200

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define TPS 120.0f
+#define TICK_DURATION 1 / TPS
+#define GRAVITY -36.0f 
+#define WINDOW_HEIGHT 640.0f
+#define WINDOW_WIDTH 640.0f
+
+#endif // CONFIG_H
